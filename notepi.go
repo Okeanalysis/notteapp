ec0daa6b66d6004e726e88c222133322ef8e862d
Okeanalysis <emmanueloke760@gmail.com>
2025-04-03 16:10:11 +0100

    noteapi

---
+++ notepi.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"sync"
+)
+
+type NOTE struct {
+	Num      int    `json:"num"`
+	Title    string `json:"title"`
+	Content  string `json:"content"`
+	Category string `json:"category"`
+}
+
+var (
+	Notee   = []NOTE{}
+	nextNum = 1
+	mu      sync.Mutex
+)
+
+func getnote(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	mu.Lock()
+	defer mu.Unlock()
+	json.NewEncoder(w).Encode(Notee)
+}
+
+func creattenote(w http.ResponseWriter, r *http.Request) {
+	var note NOTE
+	if err := json.NewDecoder(r.Body).Decode(&note); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	mu.Lock()
+	note.Num = nextNum
+	nextNum++
+	Notee = append(Notee, note)
+	mu.Unlock()
+
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(note)
+
+}
+
+func main() {
+	http.HandleFunc("/note", getnote)
+	http.HandleFunc("/note/create", creattenote)
+
+	fmt.Println("server running")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Println("error loading server:", err)
+	}
+}
