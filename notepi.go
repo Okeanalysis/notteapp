82b4c9ad622a9aff9f2e7976fed44498df62f0ac
Okeanalysis <emmanueloke760@gmail.com>
2025-04-03 23:25:57 +0100

    update

--- notepi.go
+++ notepi.go
@@ -47,23 +47,69 @@ func creattenote(w http.ResponseWriter, r *http.Request) {
 
 func updattenote(w http.ResponseWriter, r *http.Request) {
 	var num int
+
 	if err := json.NewDecoder(r.Body).Decode(&num); err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	numStr := r.URL.Path[len("/note/update/")]
+	numStr := r.URL.Path[len("/note/update/"):]
 
 	num, err := strconv.Atoi(numStr)
 	if err != nil {
-		http.Error
+		http.Error(w, "Invalid note number", http.StatusBadRequest)
+		return
 	}
 
+	var updatednote NOTE
+	if err := json.NewDecoder(r.Body).Decode(&updatednote); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	mu.Lock()
+	defer mu.Unlock()
+
+	for i, note := range Notee {
+		if note.Num == num {
+			Notee[i].Title = updatednote.Title
+			Notee[i].Content = updatednote.Content
+			Notee[i].Category = updatednote.Category
+
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(Notee[i])
+			return
+		}
+
+	}
+
+	http.Error(w, "Note not found", http.StatusNotFound)
+
+}
+
+func removenote(w http.ResponseWriter, r *http.Request) {
+	var num int
+	if err := json.NewDecoder(r.Body).Decode(&num); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	mu.Lock()
+	defer mu.Unlock()
+	for i, note := range Notee {
+		if note.Num == num {
+			Notee = append(Notee[:i], Notee[i+1:]...)
+			w.WriteHeader(http.StatusNoContent)
+			return
+
+		}
+	}
+	http.Error(w, "Note not found", http.StatusNotFound)
 }
 
 func main() {
 	http.HandleFunc("/note", getnote)
 	http.HandleFunc("/note/create", creattenote)
+	http.HandleFunc("/note/update", updattenote)
+	http.HandleFunc("/note/remove", removenote)
 
 	fmt.Println("server running")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
